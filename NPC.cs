e173227fb98c71b902681bd9353305fa8afbf6bf
mastermune <masamunedragon@gmail.com>
2020-01-23 22:28:42 -0700

    Seliana NPC default locations
    new formattting/ordering for npclist.xml as well

--- NPC.cs
+++ NPC.cs
@@ -9,28 +9,29 @@ namespace MHWRoommates
         [XmlElement("NPCID")]
         public int NpcID { get; set; }
 
-        [XmlElement("Animation")]
-        public UInt32 Animation { get; set; }
-
         [XmlElement("Name")]
         public string Name { get; set; }
 
-        [XmlElement("Warning")]
-        public string Warning { get; set; }
-
-        public int Index { get; set; }
-
-        [XmlArray("Animations")]
-        [XmlArrayItem("Animation")]
-        public List<UInt32> Animations { get; set; }
+        [XmlArray("Rooms")]
+        [XmlArrayItem("Room")]
+        public List<string> DefaultRooms { get; set; }
 
         [XmlArray("Positions")]
         [XmlArrayItem("Position")]
         public List<Room.Point3D> DefaultPositions { get; set; }
 
-        [XmlArray("Rooms")]
-        [XmlArrayItem("Room")]
-        public List<string> DefaultRooms { get; set; }
+        [XmlElement("Animation")]
+        public UInt32 Animation { get; set; }
+
+        [XmlArray("Animations")]
+        [XmlArrayItem("Animation")]
+        public List<UInt32> Animations { get; set; }
+
+        [XmlElement("Warning")]
+        public string Warning { get; set; }
+
+        [XmlIgnore]
+        public int Index { get; set; }
 
         public override string ToString()
         {
